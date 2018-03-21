d05410b550c90ffa7333d3aa979e8ff7faccacc0
yuehaowang <wangyuehao1999@gmail.com>
2018-03-22 01:12:10 +0800

    Rename: GameManage => SceneManage.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class SceneManager : MonoBehaviour {
     GameController.localPlayer localPlayerController;
     GameController.otherPlayer otherPlayerController;
 
