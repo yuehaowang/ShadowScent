9d51cfe610017091d8b75e229e5e1fba1a4fb1b0
yuehaowang <wangyuehao1999@gmail.com>
2018-03-22 19:52:56 +0800

    Rename: SceneManager => LevelManage for conflict class name.

--- Assets/Scripts/LevelManage.cs
+++ Assets/Scripts/LevelManage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour {
+public class LevelManage : MonoBehaviour {
     GameController.localPlayer localPlayerController;
     GameController.otherPlayer otherPlayerController;
 
