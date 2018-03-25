2fa07ba3ccb0d2cd38a0f3f4652649ac0a8ae783
Xiayc <xiayc@shanghaitech.edu.cn>
2018-03-25 22:21:15 +0800

    network without bugs

--- Assets/Scripts/Networking/DataSync.cs
+++ Assets/Scripts/Networking/DataSync.cs
@@ -4,128 +4,129 @@ using System.Threading.Tasks;
 using UnityEngine;
 using System;
 
-public static class DataSync {
+public static class DataSync
+{
 
-	private static IMobileServiceTable<CharacterData> characterDataTable_useProperty;
+    private static IMobileServiceTable<CharacterData> characterDataTable_useProperty;
 
-	// get current time stamp, the function is derieved from internet
-	private static long GetCurrentTime()  
-	{  
-		TimeSpan cha = (DateTime.Now - TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)));  
-		long t = (long)cha.TotalSeconds;  
-		return t;  
-	}
+    // get current time stamp, the function is derieved from internet
+    private static long GetCurrentTime()
+    {
+        TimeSpan cha = (DateTime.Now - TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)));
+        long t = (long)cha.TotalSeconds;
+        return t;
+    }
 
-	// upload the characterData
-	public static async Task uploadPlayerData(CharacterData cData)
-	{
-		cData.Time = GetCurrentTime();
-		// await uploadAsync(cData);
+    // upload the characterData
+    public static async Task uploadPlayerData(CharacterData cData)
+    {
+        cData.Time = GetCurrentTime();
+        // await uploadAsync(cData);
 
-		var allEntriesBefore = await updateAsync ();
-		// if successfully update the table, then insert latest data, otherwise, the upload is fail
-		if (allEntriesBefore != null) 
-		{
-			try
-			{
-				Debug.Log("Uploading player Data to Azure...\n" + DataUtils.CharacterData2String(cData));
-				await characterDataTable.InsertAsync(cData);
+        var allEntriesBefore = await updateAsync();
+        // if successfully update the table, then insert latest data, otherwise, the upload is fail
+        if (allEntriesBefore != null)
+        {
+            try
+            {
+                Debug.Log("Uploading player Data to Azure...\n" + DataUtils.CharacterData2String(cData));
+                await characterDataTable.InsertAsync(cData);
 
-				Debug.Log("Finished uploading player data.\n" + DataUtils.CharacterData2String(cData));
-			}
-			catch (System.Exception e)
-			{
-				Debug.Log("Error uploading player data: " + e.Message);
-			}
-			// clear up the historical player data for that player to avoid redundancy
-			foreach (var item in allEntriesBefore)
-			{
-				try
-				{
-					if (item.playerId == cData.playerId) await characterDataTable.DeleteAsync(item);
-				}
-				catch (Exception)
-				{
-					throw;
-				}
-			}
-			Debug.Log ("Delete OK");
-		}
+                Debug.Log("Finished uploading player data.\n" + DataUtils.CharacterData2String(cData));
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("Error uploading player data: " + e.Message);
+            }
+            // clear up the historical player data for that player to avoid redundancy
+            foreach (var item in allEntriesBefore)
+            {
+                try
+                {
+                    if (item.playerId == cData.playerId) await characterDataTable.DeleteAsync(item);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            Debug.Log("Delete OK");
+        }
 
 
-	}
+    }
 
-	// get the objective characterData
-	public static async Task<CharacterData> getPlayerData(int playerID)
-	{
-		var allEntries = await updateAsync ();
-		allEntries.Sort ((a, b) => a.Time.CompareTo (b.Time));
-		foreach (var item in allEntries)
-		{
-			if (item.playerId == playerID)
-				return item;
-		}
-		return null;
-	}
+    // get the objective characterData
+    public static async Task<CharacterData> getPlayerData(int playerID)
+    {
+        var allEntries = await updateAsync();
+        allEntries.Sort((a, b) => a.Time.CompareTo(b.Time));
+        foreach (var item in allEntries)
+        {
+            if (item.playerId == playerID)
+                return item;
+        }
+        return null;
+    }
 
-	public static async Task outputCharacterData()
-	{
-		var allEntries = await updateAsync ();
-		String output;
-		output = "CharacterData Table:\n";
-		foreach (var item in allEntries)
-		{
-			output += DataUtils.CharacterData2String (item) + "\n";
-		}
-		Debug.Log (output);
-	}
+    public static async Task outputCharacterData()
+    {
+        var allEntries = await updateAsync();
+        String output;
+        output = "CharacterData Table:\n";
+        foreach (var item in allEntries)
+        {
+            output += DataUtils.CharacterData2String(item) + "\n";
+        }
+        Debug.Log(output);
+    }
 
-	public static async Task clearCharacterData()
-	{
-		var allEntries = await updateAsync ();
-		foreach (var item in allEntries)
-		{
-			try
-			{
-				await characterDataTable.DeleteAsync(item);
-			}
-			catch (Exception)
-			{
-				throw;
-			}
-		}
+    public static async Task clearCharacterData()
+    {
+        var allEntries = await updateAsync();
+        foreach (var item in allEntries)
+        {
+            try
+            {
+                await characterDataTable.DeleteAsync(item);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
-	}
+    }
 
-	public static IMobileServiceTable<CharacterData> characterDataTable
-	{
-		get
-		{
-			if (characterDataTable_useProperty == null)
-			{
-				characterDataTable_useProperty = AzureServiceClient.Client.GetTable<CharacterData>();
-			}
+    public static IMobileServiceTable<CharacterData> characterDataTable
+    {
+        get
+        {
+            if (characterDataTable_useProperty == null)
+            {
+                characterDataTable_useProperty = AzureServiceClient.Client.GetTable<CharacterData>();
+            }
 
-			return characterDataTable_useProperty;
-		}
-	}
+            return characterDataTable_useProperty;
+        }
+    }
 
-	private static async Task<List<CharacterData>> updateAsync()
-	{
-		try
-		{
-			var allEntries = await characterDataTable.ToListAsync();
-			Debug.Assert(allEntries != null, "ToListAsync failed!");
-			return allEntries;
-		}
-		catch (Exception)
-		{
-			throw;
-		}
-	}
+    private static async Task<List<CharacterData>> updateAsync()
+    {
+        try
+        {
+            var allEntries = await characterDataTable.ToListAsync();
+            Debug.Assert(allEntries != null, "ToListAsync failed!");
+            return allEntries;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
-	private static async Task uploadAsync(CharacterData cData)
-	{
-		
-	}
+    private static async Task uploadAsync(CharacterData cData)
+    {
+
+    }
 }
--- Assets/Scripts/Networking/NetworkManage.cs
+++ Assets/Scripts/Networking/NetworkManage.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
+﻿using Microsoft.WindowsAzure.MobileServices;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using System;
+using System.Threading;
+using UnityEngine.UI;
 
-public class NetworkManage : MonoBehaviour {
+public class NetWorkManage : MonoBehaviour
+{
+    // Use this for initialization
+    public int localPlayerIdentity = 0;
+    private CharacterData p0Data = DataUtils.emptyCharacterData(0);
+    private CharacterData p1Data = DataUtils.emptyCharacterData(1);
+    public Task upTask;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        networkLoop();
+    }
+
+    private async void networkLoop()
+    {
+        this.upTask = DataSync.uploadPlayerData(p0Data);
+        while (true)
+        {
+            if (upTask.IsCompleted)
+            {
+                this.p0Data = DataUtils.emptyCharacterData(0);
+                await this.upTask;
+                upTask = DataSync.uploadPlayerData(p0Data);
+            }
+            await Task.Delay(100);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public CharacterData getCharacterData(int playerID)
+    {
+        if (playerID == 0)
+        {
+            return this.p0Data;
+        }
+        else if (playerID == 1)
+        {
+            return this.p1Data;
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
